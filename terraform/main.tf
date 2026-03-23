4885b74f686bd67c92b61b256c61a9f28695644a
piyushrajawat1310 <piyushrajawat1310@gmail.com>
2026-03-23 15:54:46 +0000

    pushing code

---
+++ terraform/main.tf
@@ -0,0 +1,72 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+# Get latest Amazon Linux 2 AMI
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  owners = ["amazon"]
+}
+
+# Security Group
+resource "aws_security_group" "strapi_sg" {
+  name        = "strapi-sg"
+  description = "Allow SSH, HTTP, Strapi"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 1337
+    to_port     = 1337
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 Instance
+resource "aws_instance" "strapi_ec2" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t2.micro"
+
+  key_name = var.key_name
+
+  vpc_security_group_ids = [aws_security_group.strapi_sg.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              amazon-linux-extras install docker -y
+              systemctl start docker
+              systemctl enable docker
+              usermod -aG docker ec2-user
+              EOF
+
+  tags = {
+    Name = "Strapi-Server"
+  }
+}
